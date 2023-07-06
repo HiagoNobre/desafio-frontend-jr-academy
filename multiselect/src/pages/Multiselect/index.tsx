053bdec2474be87f851ed3fa19a19669bda8014f
Hiago <nobrehiago21@hotmail.com>
2023-07-06 20:53:49 -0300

    fixing scope error

--- multiselect/src/pages/Multiselect/index.tsx
+++ multiselect/src/pages/Multiselect/index.tsx
@@ -3,15 +3,15 @@ import data from '../../data';
 import './styles.css';
 
 export default function Multiselects() {
-  const {data, isModal, focus, isOpenModal} = data.demo[0]
+  const config = data.demo[0]
   
   return (
     <div className='main'>
           <Multiselect
-            items={data}
-            focus={focus}
-            isModal={isModal}
-            isOpenModal={isOpenModal}
+            items={config.data}
+            focus={config.focus}
+            isModal={config.isModal}
+            isOpenModal={config.isOpenModal}
           />
     </div>
   )
